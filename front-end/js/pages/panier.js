50eb1931ea68e4f726c9a044a993103f83564e02
Jordan Bastin <jordan.bastin@outlook.com>
2021-07-03 16:38:06 +0200

    :fix: amélioration du panier - si vide affiche paneir vide.

--- front-end/js/pages/panier.js
+++ front-end/js/pages/panier.js
@@ -1,7 +1,35 @@
 //appel de la fonction basketPreview
 basketPreview();
 
-// affichage du panier dans le tableau
+if (basketContent == 0) {
+  document.getElementById("basketEmpty").textContent = "est vide";
+} else {
+  //------------- TAB HEAD --------------
+  //récupération di template head tableau
+  const templateHeadElt = document.getElementById("productHeadList");
+  // clonage du template head tableau
+  const cloneHeadElt = document.importNode(templateHeadElt.content, true);
+  // affiachage du template head tableau
+  document.getElementById("productsList").appendChild(cloneHeadElt);
+
+  //------------- TAB TOTAL PRICE --------------
+  //récupération di template total tableau
+  const templatePriceElt = document.getElementById("templateTotalPrice");
+  // clonage du template total tableau
+  const clonePriceElt = document.importNode(templatePriceElt.content, true);
+  // affiachage du template total tableau
+  document.getElementById("totalPrice").appendChild(clonePriceElt);
+
+  //------------- FORMULAIRE DISPLAY --------------
+  //récupération di template total tableau
+  const templateFormElt = document.getElementById("templateForm");
+  // clonage du template total tableau
+  const cloneFormElt = document.importNode(templateFormElt.content, true);
+  // affiachage du template total tableau
+  document.getElementById("orderPurchase").appendChild(cloneFormElt);
+
+  //------------- TAB PRODUCT--------------
+  // affichage du panier dans le tableau
   basketContent.forEach((product) => {
     //récupération du template tableau panier
     const templateElement = document.getElementById("productTemplate");
@@ -12,17 +40,22 @@ basketPreview();
     //ajout des produits
     cloneElement.getElementById("productName").textContent = product.name;
     cloneElement.getElementById("productLenses").textContent = product.option;
-    cloneElement.getElementById("productQuantity").textContent = product.quantity;
-    cloneElement.getElementById("priceUnit").textContent = `${product.price / 100}.00 €`;
-    cloneElement.getElementById("productPrice").textContent = `${(product.quantity * product.price) / 100}.00 €`;
+    cloneElement.getElementById("productQuantity").textContent =
+      product.quantity;
+    cloneElement.getElementById("priceUnit").textContent = `${
+      product.price / 100
+    }.00 €`;
+    cloneElement.getElementById("productPrice").textContent = `${
+      (product.quantity * product.price) / 100
+    }.00 €`;
 
     //affichage du template
     document.getElementById("productsList").appendChild(cloneElement);
 
     //initialisation de total
     let total = 0;
-    for (i = 0; i < basketContent.length; i++){
-      total += basketContent[i].price * basketContent[i].quantity /100
+    for (i = 0; i < basketContent.length; i++) {
+      total += (basketContent[i].price * basketContent[i].quantity) / 100;
     }
     //calcul et affichage du total
     document.getElementById("productTotalPrice").textContent = `${total}.00€`;
@@ -39,14 +72,14 @@ basketPreview();
       window.location.reload();
     };
   }
+}
 
 // ////////////////////////// Validation du formulaire et envoie en POST //////////////////////////
 
 const regexName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
-const regexCity =
-  /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
-const regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
-const regexAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
+const regexCity =/^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
+const regexAddress = /^[0-9]{1,4}[ ,-][ A-Za-zÀ-ÿ0-9\-]+$/;
+const regexEmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
 
 document.getElementById("confirmPurchase").onclick = (event) => {
   event.preventDefault();
@@ -60,19 +93,27 @@ document.getElementById("confirmPurchase").onclick = (event) => {
   };
 
   // on valide que le formulaire soit correctement rempli
-  if (
-    (regexMail.test(contact.email) == true) &
-    (regexName.test(contact.firstName) == true) &
-    (regexName.test(contact.lastName) == true) &
-    (regexCity.test(contact.city) == true) &
-    (regexAddress.test(contact.address) == true)
-  ) {
+  if (firstName.value.length === 0 || !regexName.test(firstName.value)) {
+    alert("Merci d'entrer un prénom valide.");
+    firstName.style.borderColor = "red";
+  } else if (lastName.value.length === 0 || !regexName.test(lastName.value)) {
+    alert("Merci d'entrer un nom valide.");
+    lastName.style.borderColor = "red";
+  } else if (address.value.length === 0 || !regexAddress.test(address.value)) {
+    alert("Merci d'entrer une adresse valide.");
+    address.style.borderColor = "red";
+  } else if (city.value.length === 0 || !regexCity.test(city.value)) {
+    alert("Merci d'entrer une ville valide.");
+    city.style.borderColor = "red";
+  } else if (email.value.length === 0 || !regexEmail.test(email.value)) {
+    alert("Merci d'entrer une adresse email valide");
+    email.style.borderColor = "red";
+  } else {
     localStorage.clear();
     var products = [];
     for (let listId of basketContent) {
       products.push(listId.id);
     }
-    localStorage.clear();
     // on envoie en POST
     fetch(url + "/order", {
       method: "POST",
@@ -87,9 +128,5 @@ document.getElementById("confirmPurchase").onclick = (event) => {
       .catch((error) =>
         alert("Un des champ du formulaire n'est pas correct !")
       );
-  } else {
-    alert(
-      "Veuillez correctement renseigner l'entièreté du formulaire pour valider votre commande."
-    );
   }
-};
+}
